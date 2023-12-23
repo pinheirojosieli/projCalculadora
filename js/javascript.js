e851ad3961d07048c45ff62e69afc0746f7cd21b
Josiéli Pinheiro <112440080+pinheirojosieli@users.noreply.github.com>
2023-12-23 14:14:35 -0300

    valores adicionados estão sendo concatenados em uma nova variavel

--- js/javascript.js
+++ js/javascript.js
@@ -1,64 +1,53 @@
 let tela =  document.querySelector("input#res").value;
+let num1 = [];
+let valorNum = num1.join(''); //Concatena os valores do array
+let numRecebido = Number(valorNum); //Transforma os valores em Number
 
 
 
 
-/*
-document.addEventListener('keydown', function(event) {
-    console.log(event.altKey); // true ou false
-    console.log(event.ctrlKey); // true ou false
-    console.log(event.shiftKey); // true ou false
-    console.log(event.key); // O nome da tecla que o usuário pressionou
-    console.log(event.NONE);
 
-    var leu = event.key;
-
-  });
-*/
 
 
 function adicionarElemento (entrada){
-
-        var c = (tela+=entrada)
-      //  tela =  document.querySelector("input#res").value += entrada.replace(/[^0-9.+x-]/g, '');
-      //  console.log("MINHA TELA atual: " + c.replace(/[^0-9.+x-]/g, ''));
-        tela = document.querySelector("input#res").value = c.replace(/[^0-9.+x-]/g, '');
+        var atualizacao = (tela+=entrada);
+        tela = document.querySelector("input#res").value = atualizacao.replace(/[^0-9.+x-]/g, '');
 
 
+    
+ 
 
+      if (isNaN(entrada)) { //Verifica se o valor adicionado NÃo é um número
+     
+
+        console.log("NÃO É NUMERO");
+
+      } else {
+
+
+
+       num1.push(entrada) //Adicionar valores ao array
+
+
+        for(let i=0; i <num1.length; i++){ //Percorre o array
+ 
+          console.log("POSIÇÃO " + i + " É NUMERO  " + num1[i]);
+
+              const valorNum = num1.join(''); //Concatena os valores do array
+              const numRecebido = Number(valorNum);  //Transforma os valores em Number
+
+              console.log("aarray " + num1 );
+              console.log("valornum " + valorNum );
+              console.log("numrecebido " +  numRecebido);
+
+         }
+      }
 }
-
-
-/*
-function semLetras (a){
-
-    var c = (a)
-  //  tela =  document.querySelector("input#res").value += entrada.replace(/[^0-9.+x-]/g, '');
-  //  console.log("MINHA TELA atual: " + c.replace(/[^0-9.+x-]/g, ''));
-      console.log("MINHA TELA atual C: " + c.replace(/[^0-9.+x-]/g, ''));
-      console.log("MINHA TELA atual tela: " + tela.replace(/[^0-9.+x-]/g, ''));
-    tela = document.querySelector("input#res").value = c.replace(/[^0-9.+x-]/g, '');
-
-
-
-}
-
-*/
-
-
-
-
-
-
-
-
-
 
 
 
 function limpar (){
     tela =  document.querySelector("input#res").value = '';
-
 
 }
 
