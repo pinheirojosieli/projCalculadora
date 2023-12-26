d8365ce73c50fb78cb3af9775312863fc96da882
Josiéli Pinheiro <112440080+pinheirojosieli@users.noreply.github.com>
2023-12-26 15:29:14 -0300

    TA SOMANDO

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let tela =  document.querySelector("input#res").value;
+//let tela =  document.querySelector("input#res").value;
 let num1 = [];
-let num2 = [];
+let num2 = '';
 let num3 = [];
 let num4 = [];
 let sequencia = [];
@@ -9,122 +9,36 @@ let sequencia = [];
 
 
 
-
-
-
-
 function adicionarElemento (entrada){
-     tela = document.querySelector("input#res").value = tela+=entrada;
-     sequencia.push(entrada);
-
-     console.log("É tela: " + tela);
-
-      verSequencia();
-/*
-        for(let i=0; i <num1.length; i++){ //Percorre o array
-              console.log("aarray " + num1 );
-              console.log("valornum " + valorNum );
-              console.log("numrecebido " +  numRecebido);
-
-         }
-      }*/
-}
-
-
-function verSequencia(){
-
-
-  let valorNum = sequencia.join(''); //Concatena os valores do array
-  let numRecebido = Number(valorNum);
-  console.log("É NUM1:  " + num1);
-
-
-  //for(let i=0; i <sequencia.length; i++){ 
-    //console.log("SEQUENCIA SALVA: " + sequencia);
-
-    
-    if (!isNaN(numRecebido)) { //Verifica se o valor adicionado NÃo é um número
-   
-      num1.push(numRecebido) //Adicionar valores ao array
-
-      console.log("É NUMRECEBIDO: " + numRecebido);
-
-      } else {
-        console.log("NÃO É NUMERO: " + sequencia);
-        num1.pop();
-        numRecebido = num1.join('');
-        console.log("NÃO É NUMERO ATUAL: " + numRecebido);
-
-
-     // }
-
-
+      sequencia.push(entrada); //Adiciona os elementos digitados em um array;
+      num1 = sequencia.join(''); // OS elementos tornam-se uma string;
+      tela = document.querySelector("input#res").value = num1;
 
    }
 
-   
 
 
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function limparDigito(){
-    num1.pop();
-    
-    const valorNum = num1.join(''); //Concatena os valores do array
-    const numRecebido = Number(valorNum);  //Transforma os valores em Number
-
-    
-    console.log("aarray " + num1 );
-    console.log("valornum " + valorNum );
-    console.log("numrecebido " +  numRecebido);
-
-    //tela = document.querySelector("input#res").value = teste.replace(/[^0-9.+x-]/g, '');
-
-
-    console.log("aarray " + num1 );
-    console.log("valornum " + valorNum );
-    console.log("numrecebido " +  numRecebido);
-
+  function somar(){
+        num1 = eval(num1.replaceAll(',','.')); //CALCULA OS VALORES NA TELA
+        tela = document.querySelector("input#res").value = num1.toString().replace(".", ","); // MOSTRA O RESULTADO CONVERTENDO O '. 'EM  ', '  
+        sequencia = num1.toString().split(''); //ATUALIZA O ARRAY; 
+      
+        
+  }
   
-}
 
-
-
+  function limparDigito(){
+    sequencia.pop();
+    num1 = sequencia.join('');
+    tela =  document.querySelector("input#res").value = num1;
+  }
+  
 
   
 
-function limparTela (){
+function limparTela(){
   tela =  document.querySelector("input#res").value = '';
-  num1 = [];
+  sequencia = [];
 
 }
 
