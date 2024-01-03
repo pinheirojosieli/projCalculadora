68b293d9102f0c8c358e9bcfc141f0cdebe3c4ba
Josiéli Pinheiro <112440080+pinheirojosieli@users.noreply.github.com>
2024-01-03 18:18:36 -0300

    multiplicação ok

--- js/script.js
+++ js/script.js
@@ -1,35 +1,170 @@
 //let tela =  document.querySelector("input#res").value;
-let num1 = [];
-let num2 = '';
+let numTela = '';
+let numCalc = '';
 let num3 = [];
-let num4 = [];
+let num4 = '';
 let sequencia = [];
-//let valorNum = num1.join(''); //Concatena os valores do array
-//let numRecebido = Number(valorNum);  //Transforma os valores em Number
+
+/*
+let texto = "ABCD7EFGHIJK5LMNOPQ6RSTUVWXYZ";
+
+   let teste = [];
+   teste  = texto.split('5');
+   console.log("VALOR: " + teste);
+
+*/
+
+function quadrado (){
+      if(numCalc.indexOf("+") != -1 || numCalc.indexOf("-") != -1 || numCalc.indexOf("*") != -1 || numCalc.indexOf("/") != -1)  {     
+   
+    
+      sequencia = numCalc.split('+');
+      let part1 = sequencia.length;
+   
+      console.log("QUADRADO: " + sequencia 
+      + " TIPO: " + typeof(sequencia)
+      + " TAMANHO: " + part1   
+      + " PARTE : " + sequencia[part1-1]
+   
+      );
+   
+
+
+    //  let qua = sequencia[1]
+   
+   
+        /*  console.log(" TEM: " );
+      console.log("VALOR NUM 1 : " + numTela);
+      console.log("VALOR NUM 2 : " + numCalc );*/
+      
+      
+      } else{ // CALCULA QUANDO NÃO HÁ OPERADORES NA TELA
+        console.log("NAO TEM: " );
+        numCalc = numCalc * numCalc
+
+        console.log("NAO TEM: " + numCalc);
+      resultado(); 
+    
+      }
+  
+  
+  
+  
+  
+  
+  
+  
+    /*  numCalc += 'q';
+      sequencia = numCalc.split('q');
+      let part1 = sequencia.length;*/
+
+/*
+
+      console.log("QUADRADO: " + sequencia 
+      + " TIPO: " + typeof(sequencia)
+      + " TAMANHO: " + part1   
+      + " PARTE 2 : " + sequencia[1]
+      
+ 
+    
+    );
+
+*/
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 function adicionarElemento (entrada){
-      sequencia.push(entrada); //Adiciona os elementos digitados em um array;
-      num1 = sequencia.join(''); // OS elementos tornam-se uma string;
-      tela = document.querySelector("input#res").value = num1;
+  
 
+
+
+  numTela += entrada;
+      
+      if(entrada == ','){
+        numCalc += '.'
+     
+        console.log("VALOR é vírgula: " + numTela);
+
+
+
+      } else{
+              if(entrada == 'x'){
+
+                numCalc += '*';
+
+              }
+
+
+              else{
+                numCalc += entrada;
+              }
+
+      }
+
+    tela = document.querySelector("input#res").value = numTela;
+      
+       
+       if(numCalc.indexOf("5") != -1) {
+
+       } else {
+
+
+       }
+    
+    //console.log("VALOR NUM 1 : " + numTela);
+  //  console.log("VALOR NUM 2 : " + numCalc );
+    
    }
 
 
 
-  function somar(){
-      num1 = eval(num1.replace(',','.')); //CALCULA OS VALORES NA TELA
-      tela = document.querySelector("input#res").value = num1.toString().replace('.',','); // MOSTRA O RESULTADO CONVERTENDO O '. 'EM  ', '  
-      sequencia = num1.toString().replace('.',',').split(''); //ATUALIZA O ARRAY; 
-     
+  function resultado(){
+    
+
+        numCalc = eval(numCalc); //CALCULA OS VALORES
+        numTela = `${numCalc}`;
+
+
+        tela = document.querySelector("input#res").value = numTela.replace('.',','); // MOSTRA O RESULTADO CONVERTENDO O '. 'EM  ', '  
+        
+        numTela = tela; //ATUALIZA COM VALOR DO RESULTADO;
+
+        console.log("numTela: :  " + numTela + " TIPO "+ typeof(numTela));
+        console.log("numCalc: :  " + numCalc +" TIPO " + typeof(numCalc));
+       
+
+
+
+
+
+
+
+
+
+
   }
   
 
   function limparDigito(){
-    sequencia.pop();
-    num1 = sequencia.join('');
-    tela =  document.querySelector("input#res").value = num1;
+ 
+    
+    numTela = numTela.replace(/.$/, '');
+    numCalc = numTela.replace(',','.')
+    tela =  document.querySelector("input#res").value = numTela;
   }
   
 
@@ -37,7 +172,16 @@ function adicionarElemento (entrada){
 
 function limparTela(){
   tela =  document.querySelector("input#res").value = '';
-  sequencia = [];
+  numTela = '';
+  numCalc = '';
+
+
+  console.log("numTela: :  " + numTela + " TIPO "+ typeof(numTela));
+  console.log("numCalc: :  " + numCalc +" TIPO " + typeof(numCalc));
+
+
+
+  ;
 
 }
 
